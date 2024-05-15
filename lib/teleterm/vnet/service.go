74d8b2d2e2bc55e34cbef04efa1a12adffe4e405
Rafał Cieślak <rafal.cieslak@goteleport.com>
2024-05-15 13:21:19 +0200

    Report usage event on VNet connection

--- lib/teleterm/vnet/service.go
+++ lib/teleterm/vnet/service.go
@@ -24,12 +24,16 @@ import (
 	"sync"
 
 	"github.com/gravitational/trace"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/gravitational/teleport"
 	"github.com/gravitational/teleport/api/types"
+	prehogv1alpha "github.com/gravitational/teleport/gen/proto/go/prehog/v1alpha"
+	teletermv1 "github.com/gravitational/teleport/gen/proto/go/teleport/lib/teleterm/v1"
 	api "github.com/gravitational/teleport/gen/proto/go/teleport/lib/teleterm/vnet/v1"
 	"github.com/gravitational/teleport/lib/client"
 	"github.com/gravitational/teleport/lib/teleterm/api/uri"
+	"github.com/gravitational/teleport/lib/teleterm/clusteridcache"
 	"github.com/gravitational/teleport/lib/teleterm/daemon"
 	logutils "github.com/gravitational/teleport/lib/utils/log"
 	"github.com/gravitational/teleport/lib/vnet"
@@ -75,9 +79,18 @@ func New(cfg Config) (*Service, error) {
 }
 
 type Config struct {
+	// DaemonService is used to get cached clients and for usage reporting. If DaemonService was not
+	// one giant blob of methods, Config could accept two separate services instead.
 	DaemonService *daemon.Service
-	ClientStore   *client.Store
-	// InstallationID used for event reporting.
+	// ClientStore is needed to extract api/profile.Profile from Connect's tsh dir. Technically it
+	// could create Teleport clients as well, but we use daemon.Service for that instead since it
+	// includes a bunch of teleterm-specific necessities.
+	ClientStore *client.Store
+	// ClusterIDCache is used for usage reporting to read cluster ID that needs to be included with
+	// every event.
+	ClusterIDCache *clusteridcache.Cache
+	// InstallationID is a unique ID of this particular Connect installation, used for usage
+	// reporting.
 	InstallationID string
 }
 
@@ -89,6 +102,10 @@ func (c *Config) CheckAndSetDefaults() error {
 
 	if c.ClientStore == nil {
 		return trace.BadParameter("missing ClientStore")
+	}
+
+	if c.ClusterIDCache == nil {
+		return trace.BadParameter("missing ClusterIDCache")
 	}
 
 	if c.InstallationID == "" {
@@ -110,6 +127,23 @@ func (s *Service) Start(ctx context.Context, req *api.StartRequest) (*api.StartR
 		return &api.StartResponse{}, nil
 	}
 
+	usageReporter, err := NewUsageReporter(UsageReporterConfig{
+		ClientStore:    s.cfg.ClientStore,
+		ClientCache:    s.cfg.DaemonService,
+		EventConsumer:  s.cfg.DaemonService,
+		ClusterIDCache: s.cfg.ClusterIDCache,
+		InstallationID: s.cfg.InstallationID,
+	})
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	appProvider := &appProvider{
+		daemonService: s.cfg.DaemonService,
+		clientStore:   s.cfg.ClientStore,
+		usageReporter: usageReporter,
+	}
+
 	longCtx, cancelLongCtx := context.WithCancelCause(context.Background())
 	s.cancel = cancelLongCtx
 	defer func() {
@@ -118,11 +152,6 @@ func (s *Service) Start(ctx context.Context, req *api.StartRequest) (*api.StartR
 			cancelLongCtx(nil)
 		}
 	}()
-
-	appProvider := &appProvider{
-		daemonService: s.cfg.DaemonService,
-		clientStore:   s.cfg.ClientStore,
-	}
 
 	manager, adminCommandErrCh, err := vnet.Setup(ctx, longCtx, appProvider)
 	if err != nil {
@@ -208,6 +237,7 @@ func (s *Service) Close() error {
 type appProvider struct {
 	daemonService *daemon.Service
 	clientStore   *client.Store
+	usageReporter *usageReporter
 }
 
 func (p *appProvider) ListProfiles() ([]string, error) {
@@ -257,7 +287,22 @@ func (p *appProvider) GetDialOptions(ctx context.Context, profileName string) (*
 	return dialOpts, nil
 }
 
+// OnNewConnection submits a usage event once per appProvider lifetime.
+// That is, if a user makes multiple connections to a single app, OnNewConnection submits a single
+// event. This is to mimic how Connect submits events for its app gateways. This lets us compare
+// popularity of VNet and app gateways.
 func (p *appProvider) OnNewConnection(ctx context.Context, profileName, leafClusterName string, app types.Application) error {
+	// Enqueue the event from a separate goroutine since we don't care about errors anyway and we also
+	// don't want to slow down VNet connections.
+	go func() {
+		uri := uri.NewClusterURI(profileName).AppendLeafCluster(leafClusterName).AppendApp(app.GetName())
+
+		err := p.usageReporter.ReportApp(ctx, uri)
+		if err != nil {
+			log.ErrorContext(ctx, "Failed to submit usage event", "app", uri, "error", err)
+		}
+	}()
+
 	return nil
 }
 
@@ -289,4 +334,121 @@ func (p *appProvider) newTeleportClient(ctx context.Context, profileName, leafCl
 		return nil, trace.Wrap(err, "creating new client")
 	}
 	return tc, nil
+}
+
+type usageReporter struct {
+	cfg UsageReporterConfig
+	// reportedApps contains a set of URIs for apps which usage has been already reported.
+	// App gateways (local proxies) in Connect report a single event per gateway created per app. VNet
+	// needs to replicate this behavior, hence why it keeps track of reported apps to report only one
+	// event per app per VNet's lifespan.
+	reportedApps map[string]struct{}
+	// mu protects access to reportedApps.
+	mu sync.Mutex
+}
+
+type clientCache interface {
+	GetCachedClient(context.Context, uri.ResourceURI) (*client.ClusterClient, error)
+}
+
+type eventConsumer interface {
+	ReportUsageEvent(*teletermv1.ReportUsageEventRequest) error
+}
+
+type UsageReporterConfig struct {
+	ClientStore   *client.Store
+	ClientCache   clientCache
+	EventConsumer eventConsumer
+	// clusterIDCache stores cluster ID that needs to be included with each usage event. It's updated
+	// outside of usageReporter – the middleware merely reads data from it. If the cache does not
+	// contain the given cluster ID, usageReporter drops the event.
+	ClusterIDCache *clusteridcache.Cache
+	InstallationID string
+}
+
+func (c *UsageReporterConfig) CheckAndSetDefaults() error {
+	if c.ClientStore == nil {
+		return trace.BadParameter("missing ClientStore")
+	}
+
+	if c.ClientCache == nil {
+		return trace.BadParameter("missing ClientCache")
+	}
+
+	if c.EventConsumer == nil {
+		return trace.BadParameter("missing EventConsumer")
+	}
+
+	if c.ClusterIDCache == nil {
+		return trace.BadParameter("missing ClusterIDCache")
+	}
+
+	if c.InstallationID == "" {
+		return trace.BadParameter("missing InstallationID")
+	}
+
+	return nil
+}
+
+func NewUsageReporter(cfg UsageReporterConfig) (*usageReporter, error) {
+	if err := cfg.CheckAndSetDefaults(); err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return &usageReporter{
+		cfg:          cfg,
+		reportedApps: make(map[string]struct{}),
+	}, nil
+}
+
+func (r *usageReporter) ReportApp(ctx context.Context, appURI uri.ResourceURI) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, hasAppBeenReported := r.reportedApps[appURI.String()]; hasAppBeenReported {
+		log.DebugContext(ctx, "App was already reported", "app", appURI.String())
+		return nil
+	}
+
+	rootClusterURI := appURI.GetRootClusterURI()
+	client, err := r.cfg.ClientCache.GetCachedClient(ctx, appURI)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	rootClusterName := client.RootClusterName()
+	profile, err := r.cfg.ClientStore.GetProfile(appURI.GetProfileName())
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	clusterID, ok := r.cfg.ClusterIDCache.Load(rootClusterURI)
+	if !ok {
+		return trace.NotFound("cluster ID for %q not found", rootClusterURI)
+	}
+
+	log.DebugContext(ctx, "Reporting usage event", "app", appURI.String())
+
+	err = r.cfg.EventConsumer.ReportUsageEvent(&teletermv1.ReportUsageEventRequest{
+		AuthClusterId: clusterID,
+		PrehogReq: &prehogv1alpha.SubmitConnectEventRequest{
+			DistinctId: r.cfg.InstallationID,
+			Timestamp:  timestamppb.Now(),
+			Event: &prehogv1alpha.SubmitConnectEventRequest_ProtocolUse{
+				ProtocolUse: &prehogv1alpha.ConnectProtocolUseEvent{
+					ClusterName:   rootClusterName,
+					UserName:      profile.Username,
+					Protocol:      "app",
+					Origin:        "vnet",
+					AccessThrough: "vnet",
+				},
+			},
+		},
+	})
+	if err != nil {
+		return trace.Wrap(err, "adding usage event to queue")
+	}
+
+	r.reportedApps[appURI.String()] = struct{}{}
+
+	return nil
 }
